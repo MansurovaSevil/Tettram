0e60d48116285a85f8732065fcfb315c9796ceba
MansurovaSevil <sevilam@code.edu.az>
2018-12-03 18:52:37 +0400

    Tettram

---
+++ razor.plagiyathttc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(razor.plagiyathttc.Startup))]
+namespace razor.plagiyathttc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
